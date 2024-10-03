Fixed off by one error when generating MakeVowel glottal pulses

python_auditory_toolbox/auditory_toolbox.py
@@ -333,7 +333,7 @@ def MakeVowel(sample_len, pitch, sample_rate, f1=0, f2=0, f3=0, bw=50):
   #    frequency (sample_rate).  A vector of the requested length is returned.
   y = np.zeros(sample_len, float)
   if isinstance(pitch, (int, float)):
-    points = np.arange(0, sample_len, sample_rate/pitch)
+    points = np.arange(0, sample_len-1, sample_rate/pitch)
   else:
     points = np.sort(np.asarray(pitch))
     points = points[points < sample_len-1]

python_auditory_toolbox/auditory_toolbox_torch.py
@@ -349,7 +349,7 @@ def make_vowel(sample_len: int,
   # returned.
   y = torch.zeros(sample_len, dtype=torch.float64)
   if isinstance(pitch, (int, float)):
-    points = torch.arange(0, sample_len, sample_rate /
+    points = torch.arange(0, sample_len-1, sample_rate /
                           pitch, dtype=torch.float64)
   else:
     points = torch.sort(torch.as_tensor(pitch, dtype=torch.float64))[0]